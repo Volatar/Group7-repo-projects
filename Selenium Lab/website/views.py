d7544752f68536429369c9647774eaaedbcddc86
dlingle1 <davis.lingle@gmail.com>
2023-10-11 19:37:59 -0400

    Create new routes for other webpages

--- Selenium Lab/website/views.py
+++ Selenium Lab/website/views.py
@@ -5,3 +5,23 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template("home.html")
+
+@views.route('/search')
+def search():
+    return "<p>Search</p>"
+
+@views.route('/cart')
+def cart():
+    return "<p>Cart</p>"
+
+@views.route('/payment')
+def payment():
+    return "<p>Payment</p>"
+
+@views.route('/about')
+def about():
+    return "<p>About</p>"
+
+@views.route('/contact')
+def contact():
+    return "<p>Contact</p>"
