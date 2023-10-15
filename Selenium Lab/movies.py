51fecf2d4825335bd64418c6ca85b2a923143fd2
Jason <89305357+jslee5@users.noreply.github.com>
2023-10-14 20:53:04 -0400

    Updated movies.py

--- Selenium Lab/movies.py
+++ Selenium Lab/movies.py
@@ -1,43 +1,54 @@
-movies_dict = {
-    'A': 'Avatar',
-    'B': 'Blade Runner',
-    'C': 'Casablanca',
-    'D': 'Die Hard',
-    'E': 'E.T. the Extra-Terrestrial',
-    'F': 'Forrest Gump',
-    'G': 'Gladiator',
-    'H': 'Home Alone',
-    'I': 'Inception',
-    'J': 'Jurassic Park',
-    'K': 'The Karate Kid',
-    'L': 'The Lord of the Rings',
-    'M': 'The Matrix',
-    'N': 'National Treasure',
-    'O': 'Office Space',
-    'P': 'Pulp Fiction',
-    'Q': 'The Queen',
-    'R': 'Raiders of the Lost Ark',
-    'S': 'Star Wars',
-    'T': 'Titanic',
-    'U': 'Up',
-    'V': 'V for Vendetta',
-    'W': 'The Wizard of Oz',
-    'X': 'X-Men',
-    'Y': 'You Again',
-    'Z': 'Zootopia'
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+movies = {
+    "A": "Avatar",
+    "B": "Blade Runner",
+    "C": "Casablanca",
+    "D": "Die Hard",
+    "E": "E.T. the Extra-Terrestrial",
+    "F": "Forrest Gump",
+    "G": "Gladiator",
+    "H": "Home Alone",
+    "I": "Inception",
+    "J": "Jurassic Park",
+    "K": "The Karate Kid",
+    "L": "The Lord of the Rings",
+    "M": "The Matrix",
+    "N": "National Treasure",
+    "O": "Office Space",
+    "P": "Pulp Fiction",
+    "Q": "The Queen",
+    "R": "Raiders of the Lost Ark",
+    "S": "Star Wars",
+    "T": "Titanic",
+    "U": "Up",
+    "V": "V for Vendetta",
+    "W": "The Wizard of Oz",
+    "X": "X-Men",
+    "Y": "You Again",
+    "Z": "Zootopia",
 }
 
-def search_movie():
-    while True:
-        user_input = input("Enter the first letter of the movie title (A-Z) to see what's available for rental, or 'quit' to exit: ").upper()
-        
-        if user_input == 'QUIT':
-            print("Exiting the program.")
-            break
-        
-        movie_title = movies_dict.get(user_input, 'Movie not found')
-        print(f"Movie title starting with '{user_input}': {movie_title}")
 
-# Main program
+@app.route("/", methods=["GET", "POST"])
+def movie_search():
+    result = ""
+
+    if request.method == "POST":
+        user_input = request.form["user_input"]
+        user_input = user_input.upper()
+
+        if user_input == "QUIT":
+            result = "You have quit the search."
+        elif user_input in movies:
+            result = ", ".join(movies[user_input])
+        else:
+            result = 'No movies found for the letter "{}".'.format(user_input)
+
+    return render_template("search.html", result=result)
+
+
 if __name__ == "__main__":
-    search_movie()
+    app.run(debug=True)
