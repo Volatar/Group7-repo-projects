37368a940e7524d18921238ea516a4d9dece232b
wcejahuerta <103069876+wcejahuerta@users.noreply.github.com>
2023-10-24 21:02:48 -0400

    Update error_handling.py

--- Selenium Lab/error_handling.py
+++ Selenium Lab/error_handling.py
@@ -1,3 +1,13 @@
+class ElementNotFoundException(driver):
 
+
+
+class NavigationException(driver):
+
+
+
+
+
+class DataReadException(driver):
 """Handles specific exceptions, logging a user-friendly message."""
 def handle_exception(exception_type,message):
