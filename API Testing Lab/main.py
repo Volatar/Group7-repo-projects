5ea19bd899cefe1207b83fbad1c8e68e6a65dc29
dlingle1 <davis.lingle@gmail.com>
2023-11-12 23:48:51 -0500

    Add post and delete request methods to api, some validation, and expand and fix tests for proper output

--- API Testing Lab/main.py
+++ API Testing Lab/main.py
@@ -1,22 +1,51 @@
 from flask import Flask, request
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, abort
 
+# Create app and api
 app = Flask(__name__)
 api = Api(app)
 
+# Ice cream menu dictionary
 iceCream = {1: "Vanilla", 2: "Chocolate", 3: "Strawberry", 4: "Cotton Candy", 5: "Mint Chocolate Chip"}
 
 
+# Validate whether ice cream already exists
+def abort_if_id_exists(ice_cream_id):
+    if ice_cream_id in iceCream:
+        abort(409, message="ID already exists")
+
+
+# Validate whether ice cream does not exist
+def abort_if_id_does_not_exists(ice_cream_id):
+    if ice_cream_id not in iceCream:
+        abort(404, message="ID is invalid")
+
+
+# Api resource for ice cream menu
 class IceCream(Resource):
     def get(self, ice_cream_id):
+        abort_if_id_does_not_exists(ice_cream_id)
         return {ice_cream_id: iceCream[ice_cream_id]}
+
+    def post(self, ice_cream_id):
+        abort_if_id_exists(ice_cream_id)
+        iceCream[ice_cream_id] = request.form['name']
+        return {ice_cream_id: iceCream[ice_cream_id]}, 201
 
     def put(self, ice_cream_id):
+        abort_if_id_does_not_exists(ice_cream_id)
         iceCream[ice_cream_id] = request.form['name']
-        return {ice_cream_id: iceCream[ice_cream_id]}
+        return {ice_cream_id: iceCream[ice_cream_id]}, 202
+
+    def delete(self, ice_cream_id):
+        abort_if_id_does_not_exists(ice_cream_id)
+        del iceCream[ice_cream_id]
+        return '', 203
 
 
+# Add resource to api
 api.add_resource(IceCream, "/iceCream/<int:ice_cream_id>")
 
+# Run main
 if __name__ == "__main__":
     app.run(debug=True)
--- API Testing Lab/test.py
+++ API Testing Lab/test.py
@@ -1,10 +1,21 @@
 import requests
 
+# Base url
 base = "http://127.0.0.1:5000/"
 
+# Change flavor test
 response = requests.get(base + "iceCream/4")
 print(response.json())
 response = requests.put(base + "iceCream/4", data={"name": "Cookies and Creme"})
 print(response.json())
-response = requests.get(base + "iceCream/4")
+
+# Delete flavor test
+response = requests.get(base + "iceCream/5")
+print(response.json())
+requests.delete(base + "iceCream/5")
+response = requests.get(base + "iceCream/5")
+print(response.json())
+
+# Add flavor test
+response = requests.post(base + "iceCream/5", data={"name": "Chocolate Chip Cookie Dough"})
 print(response.json())
