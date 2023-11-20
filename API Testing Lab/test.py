619eb3b4e29e63bf40926482750e32a4560876e9
Taylor Sanderson <sanderson.taylor@gmail.com>
2023-11-20 12:35:29 -0500

    never print when using pytest. Use asserts.

--- API Testing Lab/test.py
+++ API Testing Lab/test.py
@@ -6,25 +6,21 @@ base = "http://127.0.0.1:5000/"
 
 def test_get():
     response = requests.get(base + "iceCream/4")
-    print(response.json())
     assert response.status_code == 201
 
 
 def test_get_no_id():
     response = requests.get(base + "iceCream/6")
-    print(response.json())
     assert response.status_code != 201
 
 
 def test_put():
     response = requests.put(base + "iceCream/4", data={"name": "Cookies and Creme"})
-    print(response.json())
     assert response.status_code == 203
 
 
 def test_put_no_id():
     response = requests.put(base + "iceCream/6", data={"name": "Cookies and Creme"})
-    print(response.json())
     assert response.status_code != 203
 
 
@@ -40,11 +36,9 @@ def test_delete_no_id():
 
 def test_post():
     response = requests.post(base + "iceCream/5", data={"name": "Chocolate Chip Cookie Dough"})
-    print(response.json())
     assert response.status_code == 202
 
 
 def test_post_existing_id():
     response = requests.post(base + "iceCream/3", data={"name": "Chocolate Chip Cookie Dough"})
-    print(response.json())
     assert response.status_code != 202
