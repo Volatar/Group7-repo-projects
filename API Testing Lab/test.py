ae84795ba2832abc0d76236e17d646a3d46e3cb7
dlingle1 <davis.lingle@gmail.com>
2023-11-20 14:27:06 -0500

    Add test for contents of the response in api request

--- API Testing Lab/test.py
+++ API Testing Lab/test.py
@@ -6,21 +6,25 @@ base = "http://127.0.0.1:5000/"
 
 def test_get():
     response = requests.get(base + "iceCream/4")
+    assert response.json() == {"4": "Cotton Candy"}
     assert response.status_code == 201
 
 
 def test_get_no_id():
     response = requests.get(base + "iceCream/6")
+    assert response.json() == {"message": "ID is invalid"}
     assert response.status_code != 201
 
 
 def test_put():
     response = requests.put(base + "iceCream/4", data={"name": "Cookies and Creme"})
+    assert response.json() == {"4": "Cookies and Creme"}
     assert response.status_code == 203
 
 
 def test_put_no_id():
     response = requests.put(base + "iceCream/6", data={"name": "Cookies and Creme"})
+    assert response.json() == {"message": "ID is invalid"}
     assert response.status_code != 203
 
 
@@ -31,14 +35,17 @@ def test_delete():
 
 def test_delete_no_id():
     response = requests.delete(base + "iceCream/5")
+    assert response.json() == {"message": "ID is invalid"}
     assert response.status_code != 204
 
 
 def test_post():
     response = requests.post(base + "iceCream/5", data={"name": "Chocolate Chip Cookie Dough"})
+    assert response.json() == {"5": "Chocolate Chip Cookie Dough"}
     assert response.status_code == 202
 
 
 def test_post_existing_id():
     response = requests.post(base + "iceCream/3", data={"name": "Chocolate Chip Cookie Dough"})
+    assert response.json() == {"message": "ID already exists"}
     assert response.status_code != 202
